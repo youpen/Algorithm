57b076be238823925e533b3d28a77dc9ae0be50d
YupengWu <yupeng.wu@shopee.com>
2019-09-10 19:55:32 +0800

    19

--- leetcodeEditor/leetcode/editor/en/[19]Remove Nth Node From End of List.js
+++ leetcodeEditor/leetcode/editor/en/[19]Remove Nth Node From End of List.js
@@ -33,19 +33,37 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    if (!head || !head.next) return null;
-    if (n===1 && !head.next.next) { head.next = null; return head }
-    let record = []
-    let curr = head;
-    while (curr !== null) {
-        record.push(curr)
-        if (record.length > n+1) {
-            record.shift()
-        }
-        curr = curr.next
+    // if (!head || !head.next) return null;
+    // if (n===1 && !head.next.next) { head.next = null; return head }
+    // let record = []
+    // let curr = head;
+    // while (curr !== null) {
+    //     record.push(curr)
+    //     if (record.length > n+1) {
+    //         record.shift()
+    //     }
+    //     curr = curr.next
+    // }
+    // if (record.length === n) return head.next
+    // record[0].next = record[2]
+    // return head;
+
+    if (!head || !head.next) { return null }
+    // if (n===1 && !head.next.next) { head.next = null; return head }
+    // if (n===2 && !head.next.next) { return head.next }
+    let fast = head;
+    let slow = head;
+    for (let i = 1; i <= n; i++) {
+        fast = fast.next;
     }
-    if (record.length === n) return head.next
-    record[0].next = record[2]
+    if (!fast) {
+        return head.next
+    }
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+    slow.next = slow.next.next;
     return head;
 };
 //leetcode submit region end(Prohibit modification and deletion)
