45251461f20e8084c63de4721a3817d5baabb025
YupengWu <yupeng.wu@shopee.com>
2019-09-10 17:34:01 +0800

    19191919191919191919191919191919191919

---
+++ leetcodeEditor/leetcode/editor/en/[19]Remove Nth Node From End of List.js
@@ -0,0 +1,51 @@
+//Given a linked list, remove the n-th node from the end of list and return its head. 
+//
+// Example: 
+//
+// 
+//Given linked list: 1->2->3->4->5, and n = 2.
+//
+//After removing the second node from the end, the linked list becomes 1->2->3->5.
+// 
+//
+// Note: 
+//
+// Given n will always be valid. 
+//
+// Follow up: 
+//
+// Could you do this in one pass? 
+// Related Topics Linked List Two Pointers
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    if (!head || !head.next) return null;
+    if (n===1 && !head.next.next) { head.next = null; return head }
+    let record = []
+    let curr = head;
+    while (curr !== null) {
+        record.push(curr)
+        if (record.length > n+1) {
+            record.shift()
+        }
+        curr = curr.next
+    }
+    if (record.length === n) return head.next
+    record[0].next = record[2]
+    return head;
+};
+//leetcode submit region end(Prohibit modification and deletion)
