495e967225e5a84b37fb1e9158c4c09be953c95d
YupengWu <yupeng.wu@shopee.com>
2019-09-10 20:24:14 +0800

    160

---
+++ leetcodeEditor/leetcode/editor/en/[23]Merge k Sorted Lists.js
@@ -0,0 +1,33 @@
+//Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity. 
+//
+// Example: 
+//
+// 
+//Input:
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//Output: 1->1->2->3->4->4->5->6
+// 
+// Related Topics Linked List Divide and Conquer Heap
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    
+};
+//leetcode submit region end(Prohibit modification and deletion)
