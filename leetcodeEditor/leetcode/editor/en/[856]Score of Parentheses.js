25f4f23c95c3e09e7c79fc205f39058b572c5fae
youpen <woo.yupeng@gmail.com>
2019-10-08 21:48:07 +0800

    reverse stack by recursive

---
+++ leetcodeEditor/leetcode/editor/en/[856]Score of Parentheses.js
@@ -0,0 +1,67 @@
+//Given a balanced parentheses string S, compute the score of the string based on the following rule: 
+//
+// 
+// () has score 1 
+// AB has score A + B, where A and B are balanced parentheses strings. 
+// (A) has score 2 * A, where A is a balanced parentheses string. 
+// 
+//
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: "()"
+//Output: 1
+// 
+//
+// 
+// Example 2: 
+//
+// 
+//Input: "(())"
+//Output: 2
+// 
+//
+// 
+// Example 3: 
+//
+// 
+//Input: "()()"
+//Output: 2
+// 
+//
+// 
+// Example 4: 
+//
+// 
+//Input: "(()(()))"
+//Output: 6
+// 
+//
+// 
+//
+// Note: 
+//
+// 
+// S is a balanced parentheses string, containing only ( and ). 
+// 2 <= S.length <= 50 
+// 
+// 
+// 
+// 
+// 
+// Related Topics String Stack
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} S
+ * @return {number}
+ */
+var scoreOfParentheses = function(S) {
+    
+};
+//leetcode submit region end(Prohibit modification and deletion)
